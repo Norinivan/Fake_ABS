9cb359ff7aa9b00515811bd71f0c47192fd6d5d4
Ivan Norin <valdaykirov@gmail.com>
2025-10-26 19:02:22 +0300

    Add gitattributes for line endings

--- Fake_ABS/Fake_ABS/main.c
+++ Fake_ABS/Fake_ABS/main.c
@@ -9,79 +9,88 @@
 #include <xc.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#define OUTPIN	0
-#define BTNPIN	1
-#define FUSEPIN	4
-#define TESTPIN 3
-unsigned char status_blink = 0;
+#define OUTPIN	0	//Выход на светодиод на панели
+#define BTNPIN	1	//Вход кнопки диагностики
+#define FUSEPIN	4	//Вход с предохранителя ABS
+#define TESTPIN 3	//Просто тестовый пин
+unsigned char	status_blink = 0;	//Статус прохождения диагностических блинков 1,1
+unsigned int	counter = 0;	//Счётчик времени, по которому моргают диагностические коды
+unsigned char	restart = 0;	//Флаг перезагрузки по выключению предохранителя
 
-void blink(void);
+void start_blink(void);
 
 ISR(PCINT0_vect)
 {
-	if (!(PORTB & (1<<BTNPIN)))
+	if (!(PINB & (1<<BTNPIN)))	//Если нажата кнопка диагностики
 	{
-		status_blink = 1;
-		PORTB|=(1<<TESTPIN);
+		status_blink = 1;		//Выставляю флад диагностики
+		counter = 0;			//Сбрасываю счётчик времени
+		PORTB|=(1<<TESTPIN)|(1<<OUTPIN);	//Зажигаю светодиод на панели
+		while(!(PINB & (1<<BTNPIN)));		//Жду, пока нажата кнопка
+		PORTB&=~((1<<OUTPIN)|(1<<TESTPIN));	//Выключаю светодиод
 	}
-	else
+	if (!(PINB & (1<<FUSEPIN)))				//Если предохранитель выдернули
 	{
-		status_blink = 0;
-		PORTB&=~(1<<OUTPIN);
-		PORTB&=~(1<<TESTPIN);
+		PORTB|=(1<<TESTPIN)|(1<<OUTPIN);	//Зажигаю светодиод
+		while (!(PINB & (1<<FUSEPIN)));		//жду, пока не вставят предохранитель
+		PORTB&=~((1<<OUTPIN)|(1<<TESTPIN));	//Выключаю светодиод
+		restart = 1;						//Поднимаю флаг перезагрузки
 	}
-	
+		
 }
 
 int main(void)
 {
+	{
+		
 	DDRB|=(1<<OUTPIN)|(1<<TESTPIN);
 	PCMSK |= (1 << BTNPIN) | (1 << FUSEPIN);
 	GIMSK |= (1 << PCIE);
 	sei();
 	
-	//-----------Start Blink-----------
-	_delay_ms(500);
-	PORTB|=(1<<OUTPIN);
-	_delay_ms(1500);
-	PORTB&=~(1<<OUTPIN);
-	//---------------------------------
+	if (!(PINB & (1<<FUSEPIN)))	
+	{
+		PORTB|=(1<<TESTPIN)|(1<<OUTPIN);	//Зажигаю светодиод
+		while (!(PINB & (1<<FUSEPIN)));		//жду, пока не вставят предохранитель
+		PORTB&=~((1<<OUTPIN)|(1<<TESTPIN));
+	}
+	start_blink();
 	
+	}
     while(1)
     {
         if (status_blink == 1)
         {
-			while(!(PORTB & (1<<BTNPIN)))
+			_delay_ms(10);
+			counter++;
+			if (counter == 150 || counter == 350 || counter == 850 || counter == 1050)
 			{
-				PORTB|=(1<<OUTPIN);
+				PORTB |= (1<<OUTPIN)|(1<<TESTPIN);
 			}
-			PORTB&=~(1<<OUTPIN);
-			status_blink = 0;
-			//blink();
+			if (counter == 200 || counter == 400 || counter == 900 || counter == 1100)
+			{
+				PORTB&=~((1<<OUTPIN)|(1<<TESTPIN));
+			}
+			if (counter == 1100)
+			{
+				status_blink = 0;
+				counter = 0;
+			}
         }
+		if (restart == 1)
+		{
+			status_blink = 0;
+			counter = 0;
+			restart = 0;
+			start_blink();
+		}
     }
 }
 
-void blink(void)
+void start_blink(void)
 {
-unsigned int arr[8]={1500, 500, 500, 500, 1500, 500, 500, 500};
-PORTB&=~(1<<OUTPIN);	
-for(unsigned char i=0; i<8; i++)
-{
-	for (unsigned int j=arr[i]; j>0; j--)
-	{
-		_delay_ms(1);
-		if (status_blink == 1)
-		{
-			break;
-		}
-	}
-	PORTB^=(1<<OUTPIN);
-	if (status_blink == 1)
-	{
-		status_blink = 0;
-		PORTB&=~(1<<OUTPIN);
-		break;
-	}
-}
+	_delay_ms(500);
+	PORTB|=(1<<OUTPIN);
+	_delay_ms(1500);
+	PORTB&=~(1<<OUTPIN);
 }
